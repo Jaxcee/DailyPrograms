3fbf0eb488ba2d8f9cdec9ba98673bd8a31fe36f
JAGADESH J <you@example.com>
2024-02-20 17:13:51 +0530

    First Commit

--- src/com/bridgelabz/dailyprograms/Palindrome.java
+++ src/com/bridgelabz/dailyprograms/Palindrome.java
@@ -1,4 +1,19 @@
 package com.bridgelabz.dailyprograms;
 
 public class Palindrome {
+    public static void main(String[] args) {
+        String name = "racecar";
+        String rev ="";
+        for(int i =0;i<name.length();i++){
+            rev=name.charAt(i)+rev;
+
+        }
+        System.out.println(rev);
+        if(name.equals(rev)){
+            System.out.println("palindrome");
+
+        }else{
+            System.out.println("not palindrome");
+        }
+    }
 }
